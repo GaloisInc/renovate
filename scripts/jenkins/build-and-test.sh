1830bb5f11b4f4b19fe3d55bb6af751fd839519f
Tristan Ravitch <tristan@nochair.net>
2017-10-20 15:33:28 -0700

    Add a test script

---
+++ scripts/jenkins/build-and-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+export GHC_VER=$1
+
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+ABSOLUTE_DIR=$(dirname ${ABSOLUTE_PATH})
+ROOT=${ABSOLUTE_DIR}/../..
+
+# Tools
+export GHC=/opt/ghc/${GHC_VER}/bin
+CABAL=/opt/cabal/2.0/bin
+HAPPY=/opt/happy/1.19.5/bin
+ALEX=/opt/alex/3.1.7/bin
+
+pushd "${ROOT}"
+
+echo Building on "$(hostname -f)"
+/sbin/ifconfig -a
+
+echo "Updating submodules"
+git submodule update --init
+
+export PATH=$GHC:$CABAL:$HAPPY:$ALEX:$PATH
+
+echo "Environment is:"
+printenv
+
+echo "Updating hackage index"
+cabal update || exit 1
+
+echo "Testing ghc"
+/opt/ghc/${GHC_VER}/bin/ghc --version
+
+cabal new-build renovate-x86 || exit 1
+cabal new-build renovate-ppc || exit 1
