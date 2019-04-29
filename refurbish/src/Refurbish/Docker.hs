12dc23d8eb9af85e8c90b06eb4c2c2bffa9a5628
Nathan Collins <conathan@galois.com>
2019-04-29 13:21:49 -0700

    Improve comments and document a gotcha

--- refurbish/src/Refurbish/Docker.hs
+++ refurbish/src/Refurbish/Docker.hs
@@ -21,22 +21,41 @@ dockerFile =
 
 initializeQemuRunner :: IO (Either (Int, String, String) Runner)
 initializeQemuRunner = do
+  -- WARNING: if the output is not a valid 'String' in the current
+  -- encoding then the 'hGetcontents' call in the implementation
+  -- 'P.readProcessWithExitCode' here can fail with
+  --
+  -- > hGetContents: invalid argument (invalid byte sequence)
+  --
+  -- Ran into this when testing @gzip@ with compressed file going to
+  -- stdout.
   (ec, out, err) <- P.readProcessWithExitCode "docker" ["build", "-t", qemuRunnerName, "-"] dockerFile
   case ec of
     E.ExitSuccess -> do return (Right (Runner runner))
     E.ExitFailure rv -> return (Left (rv, out, err))
 
--- | * A mapping of local filenames to in-container filenames - all must be absolute
---   * Command line to run
-newtype Runner = Runner { runInContainer :: [(FilePath, FilePath)] -> [String] -> IO (E.ExitCode, String, String) }
+-- | Run a command in a docker container.
+--
+-- The arguments are
+--
+-- * A mapping of local filenames to in-container filenames - all must be absolute
+-- * Command line to run, e.g. @["/bin/ls", "-l", ".."]@.
+--
+-- Returns the exit code, stdout, and stderr.
+newtype Runner = Runner {
+  runInContainer :: [(FilePath, FilePath)]
+                 -> [String]
+                 -> IO (E.ExitCode, String, String)
+  }
 
+-- | An implementation of the 'Runner' newtype.
 runner :: [(FilePath, FilePath)] -> [String] -> IO (E.ExitCode, String, String)
-runner mapping args =
+runner mapping cmdline =
   P.readProcessWithExitCode "docker" dargs ""
   where
     argMap = [["-v", src ++ ":" ++ dst] | (src, dst) <- mapping]
     dargs = concat [ ["run", "--init", "--rm"]
                    , concat argMap
                    , [qemuRunnerName]
-                   , args
+                   , cmdline
                    ]
